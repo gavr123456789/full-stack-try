53ad596f0a82aedf74daac67c1f2fcb1cc760bab
g.maksutenko <g.maksutenko@indevstudio.com>
2022-01-11 18:58:11 +0300

    fix nimongo

--- nimFailed/backend/mongoViews.nim
+++ nimFailed/backend/mongoViews.nim
@@ -1,24 +1,34 @@
-import nimongo.bson  ## MongoDB BSON serialization/deserialization
-import nimongo.mongo ## MongoDB client
 import prologue
-
-
+import nimongo/bson
+import nimongo.mongo 
+# import asyncdispatch
 
 var
   m: mongo.Mongo = newMongoWithURI("mongodb://sas:sas@localhost:27017/test")
-  mBase: MongoBase = m.slaveOk(true).allowPartial(false)
+  # mBase: MongoBase = m.slaveOk(true).allowPartial(false)
 
 
 var collection {.threadvar.} : Collection[mongo.Mongo] 
 collection = m["test"]["users2"]
 
 
-proc getUserByNameMongo*(ctx: Context) {.async gcsafe.} = 
+
+discard m.connect()
+# let sas2 = collection.find( bson.`%*`({"name": "qwe"})).all()
+# echo m["test"].listCollections()
+# echo "sas2 = ", sas2
+
+
+proc getUserByNameMongo*(ctx: Context) {.gcsafe, async.} = 
   let nameParam = ctx.getPathParams("name")
   echo "nameParam = ", nameParam
 
-  let sas = collection.find( bson.`%*`({"name": nameParam}) )
-  if not sas.isNil:
-    echo "sas.isNil: ", sas.isNil
-    echo sas.count
-  resp "sas"
+  let sas = collection.find(bson.`%*`({"name": nameParam})).all()
+  echo sas
+  # collection.insert( bson.`%*`({"name": nameParam}))
+  # if not sas.isNil:
+    # echo "sas.isNil: ", sas.isNil
+    # echo sas.count
+  # echo "result = ", sas
+
+  resp $sas
